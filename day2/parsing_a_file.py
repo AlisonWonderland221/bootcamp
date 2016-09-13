cd8b431df090940d2e62e9d632455d573d224b0f
Alison Koontz <akoontz@caltech.edu>
2016-09-12 20:25:57 -0700

    completed parsing_a_file.py

--- day2/parsing_a_file.py
+++ day2/parsing_a_file.py
@@ -3,4 +3,6 @@ import os
 with open('salmonella_spi1_region.fna', 'r') as f, open('salmonella.txt', 'w') as f_out:
     lines = f.readlines()
     for line in lines:
-        f_out.write(line)
+        line = line.rstrip()
+        if len(line) <= 80:
+            f_out.write(line)
