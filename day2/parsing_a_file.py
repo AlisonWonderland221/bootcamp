ed84758d77e81e42aa0decb15fab7b7c3a5c29cb
Alison Koontz <akoontz@caltech.edu>
2016-09-12 21:32:48 -0700

    Actually finished gc_blocks function

--- day2/parsing_a_file.py
+++ day2/parsing_a_file.py
@@ -1,5 +1,6 @@
 import os
 
+#takes input file, returns .txt file with sequence as single string
 with open('salmonella_spi1_region.fna', 'r') as f, open('salmonella.txt', 'w') as f_out:
     lines = f.readlines()
     for line in lines:
--- day2/pathogenicity_islands.py
+++ day2/pathogenicity_islands.py
@@ -2,7 +2,23 @@ def gc_blocks(seq, block_size):
 
     seq_list = ()
 
+    #Generates subsequences
     for i in range(len(seq) // block_size):
         seq_list = seq_list + (seq[(i * block_size):((i * block_size) + block_size)],)
 
-    return seq_list
+    #Finding GC content
+
+    gc_list = ()
+
+    for i in range(len(seq) // block_size):
+        working_string = seq_list[i]
+        count = 0
+        for j in range(block_size):
+            if working_string[j] == 'G':
+                count += 1
+            if working_string[j] == 'C':
+                count += 1
+        gc_content = count / block_size
+        gc_list = gc_list + (gc_content,)
+
+    return gc_list
